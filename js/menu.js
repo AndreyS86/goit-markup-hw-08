cf279da368b29879776ceca9900e01fa91cd3932
AndreyS86 <108714524+AndreyS86@users.noreply.github.com>
2022-09-14 12:55:58 +0300

    14,09

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,19 @@
       openMenuBtn: document.querySelector("[data-menu-open]"),
       closeMenuBtn: document.querySelector("[data-menu-close]"),
       menu: document.querySelector("[data-menu]"),
+      menuList: document.querySelector(".mob-menu__list"),
     };
   
     refs.openMenuBtn.addEventListener("click", toggleMenu);
     refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    refs.menuList.addEventListener("click", removeMenu);
   
     function toggleMenu() {
       refs.menu.classList.toggle("is-hidden");
+      refs.menu.classList.toggle("no-scroll");
+
+    } function removeMenu() {
+      refs.menu.classList.add("is-hidden");
+      refs.menu.classList.remove("no-scroll");
     }
   })();
